c8b120f2faa60f9dc26ab1505c80980d6cc516cd
Einherjar <rodrigo.silva1186@etec.sp.gov.br>
2024-06-12 00:13:15 -0300

    Improving usability

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,7 @@ import Home from "../views/home.vue";
 import Feed from "../views/feed.vue";
 import Settings from "../views/settings.vue";
 import Profile from "../views/profile.vue";
-import Votting from "../views/votting.vue";
+import Vote from "../views/votting.vue";
 import Faucet from "../views/faucet.vue";
 import { useAccountStore } from "../store/account.js";
 
@@ -13,7 +13,7 @@ const router = createRouter({
   history: createWebHashHistory(),
   routes: [
     { path: "/", component: Home, meta: { auth: false } },
-    { path: "/vote", component: Votting, meta: { auth: false } },
+    { path: "/vote", component: Vote, meta: { auth: false } },
     { path: "/faucet", component: Faucet, meta: { auth: false } },
     { path: "/user/feed", component: Feed, meta: { auth: true } },
     { path: "/user/connect", component: Connect, meta: { auth: false } },
@@ -25,7 +25,12 @@ const router = createRouter({
 router.beforeEach((to, from, next) => {
   const accountStore = useAccountStore();
   const { account } = storeToRefs(accountStore);
-  if (to.meta?.auth && !account.value.hasAccount) next("/connect");
+  if (to.meta?.auth && !account.value.hasAccount) {
+    return next("/user/connect");
+  }
+  if (to.fullPath == "/" && account.value.hasAccount) {
+    return next(`/user/${account.value.wallet}`);
+  }
   next();
 });
 
